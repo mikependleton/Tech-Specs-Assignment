1d5e807fc482372f2440a3bee4eb0892bc166498
Mike Pendleton <michaelpendleton80@yahoo.com>
2022-11-06 14:37:02 -0600

    commit for project

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-f5$cys=t$bl^!tn$4z@ho=qb9(@m&@^(vf0l0bv^r_b*w810rc"
+# SECRET_KEY = "django-insecure-f5$cys=t$bl^!tn$4z@ho=qb9(@m&@^(vf0l0bv^r_b*w810rc"
+SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -79,12 +80,13 @@ WSGI_APPLICATION = "django_project.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+# DATABASES = {
+#    "default": {
+#        "ENGINE": "django.db.backends.sqlite3",
+#        "NAME": BASE_DIR / "db.sqlite3",
+#    }
+# }
+DATABASES = {"default": env.dj_db_url("DATABASE_URL")}
 
 
 # Password validation
